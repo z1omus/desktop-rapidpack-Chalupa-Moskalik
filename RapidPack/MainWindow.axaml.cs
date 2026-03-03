cb76441975214b743dc08b7ff97a6076b02f3e39
Mania <maria.chalupa@cyfroweszkoly.pl>
2026-03-03 16:01:19 +0100

    Created method GetPrice and IsOverweight

--- RapidPack/MainWindow.axaml.cs
+++ RapidPack/MainWindow.axaml.cs
@@ -1,17 +1,36 @@
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Utilities;
 
 namespace RapidPack;
 
 public partial class MainWindow : Window
 {
+
+    private TextBlock _weight;
+    private TextBlock _length;
+    private TextBlock _height;
+    private TextBlock _width;
+    private CheckBox _express;
+    private ComboBox _type;
+    
+    
     public MainWindow()
     {
         InitializeComponent();
     }
 
-    private void SubmitButton_Click(object? sender, RoutedEventArgs e)
+    public void SubmitButton_Click(object? sender, RoutedEventArgs e)
     {
-        throw new System.NotImplementedException();
+        var weight  = int.TryParse(_weight.Text,  out var parsedWeight) ? parsedWeight : 0;
+        var length = int.TryParse(_length.Text,  out var parsedLength) ? parsedWeight : 0;
+        var height = int.TryParse(_height.Text,  out var parsedHeight) ? parsedWeight : 0;
+        var width = int.TryParse(_width.Text,  out var parsedWidth) ? parsedWeight : 0;
+        var express = Express.IsChecked == true? "ekspresowa": "zwykła";
+        var type = (Type.SelectedItem as ComboBoxItem)?.Content?.ToString();
+        var calculator = new ParcelCalculator();
+        Summary.Text = $"podsumowanie{calculator.GetPrice(weight, length, height, width, express, type)}\n{calculator.IsOverweight(weight)}";
     }
+
+    
 }
--- RapidPack/ParcelCalculator.cs
+++ RapidPack/ParcelCalculator.cs
@@ -1,42 +1,30 @@
+using System;
+using Avalonia.Controls;
+
 namespace RapidPack;
 
     public class ParcelCalculator
     {
-        private double _weight;
-        private double _length;
-        private double _height;
-        private double _width;
-        private bool _express;
-        private string _type;
 
-        public ParcelCalculator(double weight, double length, double height, double width, bool express, string type)
+        public string? IsOverweight(double weight)
         {
-            _weight = weight;
-            _length = length;
-            _height = height;
-            _width = width;
-            _express = express;
-            _type = type;
-        }
-
-        public string? IsOverweight()
-        {
-            if (_weight > 30)
+            if (weight > 30)
                 return ("Za duża waga!!!!!!!");
             return null;
         }
 
-        public double GetPrice()
+        public double GetPrice(int weight,int length,int height, int width, string express, string type)
         {
-            double price = 10 + (_weight * 2);
+            double price = 10;
+            /*double price = 10 + (weight * 2);
 
-            if ((_width + _length + _height) > 150)
-                price *= 1.5;
-            if (_express)
+            if ((width + length + height) > 150)
+                price *= 1.5;*/
+            if (express=="ekspresowa")
                 price += 15;
-            if (_type == "Fragile")
+            if (type == "Fragile")
                 price += 10;
-            if (_type == "Pallet")
+            if (type == "Pallet")
                 price = 100;
             
             return price;
