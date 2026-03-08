5c98333c1004f7b7e91c66a27ede30a3f79aeb0a
z1omus <maria.chalupa@cyfroweszkoly.pl>
2026-03-08 13:24:43 +0100

    Added live check for overweight parcels

--- RapidPack/MainWindow.axaml.cs
+++ RapidPack/MainWindow.axaml.cs
@@ -11,32 +11,35 @@ public partial class MainWindow : Window
     public TextBox _length;
     public TextBox _height;
     public TextBox _width;
-    public CheckBox _express;
-    public ComboBox _type;
     
     public MainWindow()
     {
         InitializeComponent();
-
-        _weight = this.FindControl<TextBox>("Weight");
-        _length = this.FindControl<TextBox>("Length");
-        _height = this.FindControl<TextBox>("Height");
-        _width = this.FindControl<TextBox>("Width");
-        _express = this.FindControl<CheckBox>("Express");
-        _type = this.FindControl<ComboBox>("Type");
     }
 
     public void SubmitButton_Click(object? sender, RoutedEventArgs e)
     {
-        var weight = double.TryParse(_weight.Text,  out var parsedWeight) ? parsedWeight : 0;
-        var length = double.TryParse(_length.Text,  out var parsedLength) ? parsedWeight : 0;
-        var height = double.TryParse(_height.Text,  out var parsedHeight) ? parsedWeight : 0;
-        var width = double.TryParse(_width.Text,  out var parsedWidth) ? parsedWeight : 0;
+        double.TryParse(_weight.Text,  out var parsedWeight);
+        double.TryParse(_length.Text,  out var parsedLength);
+        double.TryParse(_height.Text,  out var parsedHeight);
+        double.TryParse(_width.Text,  out var parsedWidth);
         var express = Express.IsChecked == true? "ekspresowa": "zwykła";
         var type = (Type.SelectedItem as ComboBoxItem)?.Content?.ToString();
         var calculator = new ParcelCalculator();
         Summary.Text = $"podsumowanie\n cena: {calculator.GetPrice(parsedWeight, parsedLength, parsedHeight, parsedWidth, express, type)}\nwaga: {parsedWeight}\nwymiary: {parsedLength} x {parsedHeight} x {parsedWidth}\ndostawa :{express}\ntyp przesyłki:  {type}";
     }
 
-    
+
+    private void Weight_OnTextChanging(object? s, TextChangingEventArgs e)
+    {
+        var box = s as TextBox;
+
+        if (int.TryParse(box.Text, out int weight))
+        {
+            if (weight > 30)
+            {
+                Overweight.Text = "Za duża waga!";
+            }
+        }
+    }
 }
