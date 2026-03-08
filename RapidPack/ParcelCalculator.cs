3fbb30ef9a2c6b47112380946d21cfeca2565f50
z1omus <maria.chalupa@cyfroweszkoly.pl>
2026-03-08 13:08:14 +0100

    Fixed variables problems in GetPrice()

--- RapidPack/ParcelCalculator.cs
+++ RapidPack/ParcelCalculator.cs
@@ -3,30 +3,50 @@ using Avalonia.Controls;
 
 namespace RapidPack;
 
-    public class ParcelCalculator
+public class ParcelCalculator
+{
+
+    public bool IsOverweight(double parsedWeight, string type)
     {
-
-        public string? IsOverweight(double weight)
-        {
-            if (weight > 30)
-                return ("Za duża waga!!!!!!!");
-            return null;
-        }
-
-        public double GetPrice(int weight,int length,int height, int width, string express, string type)
-        {
-            double price = 10;
-            /*double price = 10 + (weight * 2);
-
-            if ((width + length + height) > 150)
-                price *= 1.5;*/
-            if (express=="ekspresowa")
-                price += 15;
-            if (type == "Fragile")
-                price += 10;
-            if (type == "Pallet")
-                price = 100;
-            
-            return price;
-        }
+        if (parsedWeight > 30 && type !="Paleta")
+            return true;
+        return false;
     }
+
+    public (double, string) GetPrice(double weight,double length,double height, double width, string express, string type)
+    {
+        if(!IsOverweight(weight, type)){
+            string summary = "";
+            double price = 10 + (weight * 2);
+
+            if(type!="Paleta"){
+                if ((width + length + height) > 150)
+                {
+                    price *= 1.5;
+                    summary += "paczka gabarytowa +50% ceny\n";
+                }
+                if (express == "ekspresowa")
+                {
+                    price += 15;
+                    summary += "ekspresowa wysyłka +15zł\n";
+                }
+                if (type == "standard")
+                    summary += "przesyłka standardowa\n";
+                if (type == "Ostrożnie")
+                {
+                    price += 10;
+                    summary += "ostrożna przesyłka +10zł";
+                }
+            }
+            else
+            {
+                price = 100;
+                summary += "paleta - cena 100zł";
+            }
+
+            return (price, summary);
+        }
+
+        return (0, "Podano za wysoką wagę. Maksymalna waga wynosi 30 kg, chyba, że zamawia się paletę");
+    }
+}
