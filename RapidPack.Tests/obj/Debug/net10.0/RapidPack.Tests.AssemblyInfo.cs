733277e218c79578b8e7c1a8397ffa5272cd539f
stewcio <dawid.moskalik@cyfroweszkoly.pl>
2026-03-10 15:15:48 +0100

    poprawienie wizualnej strony aplikacji

--- RapidPack.Tests/obj/Debug/net10.0/RapidPack.Tests.AssemblyInfo.cs
+++ RapidPack.Tests/obj/Debug/net10.0/RapidPack.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RapidPack.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02925c8f41cbe4e739b7644430fb504438da34a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50705699e3f588476194f2e5001fd2e321370e01")]
 [assembly: System.Reflection.AssemblyProductAttribute("RapidPack.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RapidPack.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
